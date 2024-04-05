dcd171cc8155122feef2c10dfb79363b0df85205
Rutendo Kuuzabuwe (tendo ღ) <151543445+texndo@users.noreply.github.com>
2024-04-05 23:39:41 +0100

    fixed-a-background-colour-issue

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,26 +1,30 @@
 $(document).ready(function () {
     //* Display current date and time *\\
     var displaytime = document.querySelector("#currentDay");
-    var currentTime = day.js().format("dddd, MMMM D, YYYY, h:mm:ss a");
+    var currentTime = dayjs().format("dddd, MMMM D, YYYY, h:mm:ss a");
     displaytime.textContent = currentTime;
 }); 
 
 //* Save user input to local storage when save button is clicked *\\
 $(".saveBtn").on("click", function () {
-    var text = $(this).siblings("description").val();
-    var time = $(this).parent().attr("id");
-    localStorage.setItem(time,text);
-
+    var text = $(this).siblings(".description").val();
+    var time = $(this).parent().data("hour");
+    localStorage.setItem(time, text);
 });
 
 //* Update the background colour of the time blocks based on the current time *\\
 function updateHourlyBlocks() {
     var currentHour = dayjs().hour();
     $(".time-block").each(function () {
-        var blockHour = parseInt($(this).attr("id).split("-")[1]);"))
-        $(this).toggleClass("past", blockHour < currentHour);
-        $(this).toggleClass("present", blockHour === currentHour);
-        $(this).toggleClass("future", blockHour > currentHour);
+        var blockHour = parseInt($(this).data("hour"));
+        $(this).removeClass("past present future");
+        if (blockHour < currentHour) {
+            $(this).addClass("past");
+        } else if (blockHour === currentHour) {
+            $(this).addClass("present");
+        } else {
+            $(this).addClass("future");
+        }
     });
 }
 
@@ -29,11 +33,12 @@ updateHourlyBlocks();
 //* Display saved text from local storage in the respective time blocks *\\
 function displaySavedText() {
     $(".time-block").each(function () {
-        var blockHour = $(this).attr("id");
+        var blockHour = $(this).data("hour");
         $(this).children(".description").val(localStorage.getItem(blockHour));
     });
 }
 
 displaySavedText();
+
 
 
